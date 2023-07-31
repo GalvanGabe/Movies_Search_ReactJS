130894d63661752ef3b6b9694c80f09f4288f777
Gabriel Galvan <galvan.gabriel@hotmail.com>
2023-07-30 23:32:15 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/layout/Header';
+import Home from './components/pages/Home';
+import Search from './components/pages/Search';
+import Footer from './components/layout/Footer';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App() {
+
+  return (
+    
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/search' element={<Search/>} />
+      </Routes>
+      <Footer/>
+    </Router>
+
+  );
+
+}
+
+export default App;
